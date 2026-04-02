3a9eb265a3cabffed8911f565c08a67e1acd94b0
Dylan Ward <dylan.ward@iinet.net.au>
2026-04-02 20:54:56 +1000

    Update knexfile to import dotenv for environment variable management; add migration scripts to package.json for better database management. Remove obsolete migrate.ts file.

--- api/knexfile.ts
+++ api/knexfile.ts
@@ -1,3 +1,4 @@
-import config from './src/knexConfig';
+import 'dotenv/config';
+import config from './src/knexConfig.ts';
 
 export default config;
--- api/src/migrate.ts
+++
@@ -1,9 +0,0 @@
-import knex from 'knex';
-import config from './knexConfig';
-
-const db = knex(config);
-
-await db.migrate.latest();
-await db.destroy();
-
-console.log('Migration complete.');
